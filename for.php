663c1b6e75733731c4f2d1e7f92aefeab5fabb90
huynhquanghao98 <huynhquanghao98@gmail.com>
2021-07-02 20:15:24 +0700

    Update for.php

--- for.php
+++ for.php
@@ -50,12 +50,73 @@
 
 // Bai 4
 
-$n = 10;
-$x = 5;
-$tong = 0;
-for ($i=1; $i <= $n ; $i++) { 
-	$tong = $tong + $x * (2 * $i);
-}
-echo $tong;
+// $n = 10;
+// $x = 5;
+// $tong = 0;
+// for ($i=1; $i <= $n ; $i++) { 
+// 	$tong = $tong + $x * (2 * $i);
+// }
+// echo $tong;
 // Bai 5
+
+
+// $x = 1;
+// while ($x <= 1000) {
+// 	echo $x." ";
+// 	$x++;
+// }
+
+// for ($i = 1; $i < 100; $i += 2) { 
+// 	echo $i." ";
+// }
+// Bai 6
+
+$array = array(
+	array(
+		'name' 	=> 'Hao 1',
+		'email'	=> 'Hao1@gmail.com',
+		'age'	=>	'1'
+	),
+	array(
+		'name' 	=> 'Hao 2',
+		'email'	=> 'Hao2@gmail.com',
+		'age'	=>	'2'
+	),
+	array(
+		'name' 	=> 'Hao 3',
+		'email'	=> 'Hao3@gmail.com',
+		'age'	=>	'3'
+	),
+	array(
+		'name' 	=> 'Hao 4',
+		'email'	=> 'Hao4@gmail.com',
+		'age'	=>	'4'
+	),
+	array(
+		'name' 	=> 'Hao 5',
+		'email'	=> 'Hao5@gmail.com',
+		'age'	=>	'5'
+	),
+	array(
+		'name' 	=> 'Hao 6',
+		'email'	=> 'Hao6@gmail.com',
+		'age'	=>	'6'
+	)
+);
+$count = count($array);
+
+
 ?>
+<table border="1">
+	<?php
+		for($i=0; $i < $count; $i++){
+	?>		
+			<tr>
+				<td><?php echo $array[$i]['name']; ?></td>
+				<td><?php echo $array[$i]['email']; ?></td>
+				<td><?php echo $array[$i]['age']; ?></td>
+			</tr>
+	<?php
+		}
+	?>
+</table>
