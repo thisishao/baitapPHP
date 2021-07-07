5843124c2ed2629d860820db4ae344fd14d2edcd
huynhquanghao98 <huynhquanghao98@gmail.com>
2021-07-07 19:23:53 +0700

    Update function.php
    HaoHQ_update

--- function.php
+++ function.php
@@ -1,55 +1,60 @@
 <?php
 
-// $mang = array(
-//     321,
-//     312,
-//     3,
-//     4,
-//     67,
-//     5,
-//     45,
-//     56,
-//     5,
-//     7,
-//     6,
-//     787,
-//     8,
-//     7,
-//     2
+$mang = array(
+    321,
+    312,
+    3,
+    4,
+    5,
+    45,
+    56,
+    5,
+    7,
+    6,
+    787,
+    67,
+    8,
+    7,
+    2
       
-// );
-// $number = 67;
+);
+$number = 67;
 
-// function getNumber($mang, $number)
-// {
-//     $count = count($mang);
-
-//     for ($i=0; $i < $count; $i++) { 
-//         if ($mang[$i] == $number) {
-//             echo "có số ".$number;
-//             // echo $number;
-//         }
-//     }
-// }
-
-// echo getNumber($mang, $number);
-
-
-
-$demo = array();
-for ($i=1; $i <= 100; $i++) { 
-    $demo[$i] = $i;
-}
-
-function chiahetcho3($demo)
+function getNumber($mang, $number)
 {
-    for ($i=1; $i  < count($demo); $i ++) { 
-        if ($demo[$i] % 3 == 0) {
-            echo $demo[$i]."-";
+    $count = count($mang);
+
+    for ($i=0; $i < $count; $i++) { 
+        if ($mang[$i] == $number) {
+            echo "có số ".$number.' vi tri '.$i;
+            // echo $number;
+                return true;
         }
     }
 }
 
-chiahetcho3($demo);
+getNumber($mang, $number);
+
+// Bai 1
+
+
+// $demo = array();
+// for ($i=1; $i <= 100; $i++) { 
+//     $demo[$i] = $i;
+// }
+
+// function chiahetcho3($demo)
+// {
+//     for ($i=1; $i  < count($demo); $i ++) { 
+//         if ($demo[$i] % 3 == 0) {
+//             echo $demo[$i]."-";
+//         }
+//     }
+// }
+
+// chiahetcho3($demo);
+
+// Bai 2
+
 
 ?>
